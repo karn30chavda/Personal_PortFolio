172b3307170033c0383bf2028a4af461840ceb70
KARAN CHAVDA <kchavda944@gmail.com>
2025-09-14 10:28:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '4mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
